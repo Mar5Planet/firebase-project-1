c0b530a52f3201edacadc1aafdab9593e9f75cdc
Mar5Planet <amustaf@email.arizona.edu>
2021-01-19 12:21:03 -0700

    fire-base-added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,13 +5,13 @@ import {
   } from 'react-router-dom';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Navigation from '../Navigation';
-// import LandingPage from '../Landing';
-// import SignUpPage from '../SignUp';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
-// import PasswordForgetPage from '../PasswordForget';
-// import HomePage from '../Home';
-// import AccountPage from '../Account';
-// import AdminPage from '../Admin';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
  
 import * as ROUTES from '../../constants/routes';
 
@@ -23,13 +23,13 @@ const App = () => (
  
  
 
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} /> */}
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />  */}
+      <Route path={ROUTES.ADMIN} component={AdminPage} /> 
     </div>
   </Router>
 );
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+const PassF = () => (
+  <div>
+    <h1>PassF</h1>
+  </div>
+);
+ 
+export default PassF;
